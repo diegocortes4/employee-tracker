422d3826edcc8cb875ee62bec50ed1de74b3257c
Diego Cortes <diegocortes4@gmail.com>
2020-11-25 23:33:21 -0500

    added start and result

--- index.js
+++ index.js
@@ -17,4 +17,52 @@ const connection = mysql.createConnection({
     start();
   });
 
+  function start() {
+    inquirer
+      .prompt({
+        type: "list",
+        name: "option",
+        message: "What would you like to do?",
+        choices: [
+          "Add Department",
+          "Add Role",
+          "Add Employee",
+          "View Department",
+          "View Role",
+          "View Employee",
+          "Update Employee Role",
+          "Exit"
+        ]
+      })
+  
+      .then(function(result) {
+        console.log("You entered: " + result.option);
+        switch (result.option) {
+          case "Add Department":
+            addDepartment();
+            break;
+          case "Add Role":
+            addRole();
+            break;
+          case "Add Employee":
+            addEmployee();
+            break;
+          case "View Department":
+            viewDepartment();
+            break;
+          case "View Role":
+            viewRole();
+            break;
+          case "View Employee":
+            viewEmployee();
+            break;
+          case "Update Employee Role":
+            updateRole();
+            break;
+          case "Exit":
+            connection.end();
+            break;
+        }
+      });
+  }
   
